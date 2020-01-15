083d2de0780f6b28be79e6e04e8d74704435e83b
Sandro Torres <sandrotorres.ti@gmail.com>
2020-01-15 09:02:19 -0300

    Finalizando BackEnd

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,10 +5,12 @@ const routes = require('./routes');
 const app = express();
 
 mongoose.connect('mongodb+srv://Sandro:Mspgir23567@cluster0-ua9sc.mongodb.net/week10?retryWrites=true&w=majority', {
+  useCreateIndex: true,
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 app.use(express.json());
 app.use(routes);
+
 app.listen(3333);
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,17 +1,13 @@
 const { Router } = require('express');
-const axios = require('axios');
+const DevController = require('./controllers/DevController')
+const SearchController = require('./controllers/SearchController')
 
 const routes = Router();
 
-routes.post('/devs', async (request, response) => {
-  const { github_username } = request.body;
-  const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
-  const { name = login, avatar_url, bio } = apiResponse.data;
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
 
-  console.log(name, avatar_url, bio, github_username);
-
-  return(response.json({ message: 'Sandro Torres' }))
-})
+routes.get('/search', SearchController.index);
 
 module.exports = routes;
 
