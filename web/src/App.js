75095773873b6a26331ed1b31f1dd425d155e5ce
Sandro Torres <sandrotorres.ti@gmail.com>
2020-01-16 10:17:22 -0300

    Interface Web Concluída

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 // Componente: Bloco isolado de HTML, CSS e JS o qual não interfere no restante da aplicação.
 // Propriedade: Informações que um componente PAI passa para o componente FILHO.
@@ -8,100 +9,38 @@ import './App.css'
 import './Sidebar.css'
 import './main.css'
 
-function App() {
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
+import DevItem from './components/DevItem'
+import DevForm from './components/DevForm'
 
-        setLatitude(latitude);
-        setLongitude(longitude);
-      },
-      (err) => {
-        console.log(err)
-      },
-      {
-        timeout: 30000,
-      }
-    );
+function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('./devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
   }, [])
+  async function handleAddDev(data) {
+    const response = await api.post('./devs', data)
+
+    setDevs([...devs, response.data]);
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required/>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required/>
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required value={latitude}/>
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required value={longitude}/>
-            </div>
-          </div>
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev}/>
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/18393836?s=400&v=4" alt="Sandro Torres"></img>
-              <div className="user-info">
-                <strong>Sandro Torres</strong>
-                <span>ReactJS, React Native, Node.Js</span>
-              </div>
-            </header>
-            <p>Desenvolvedor Front-End: Node.JS, ReactJS e React Native.</p>
-            <a href="https://github.com/sjtorres">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/18393836?s=400&v=4" alt="Sandro Torres"></img>
-              <div className="user-info">
-                <strong>Sandro Torres</strong>
-                <span>ReactJS, React Native, Node.Js</span>
-              </div>
-            </header>
-            <p>Desenvolvedor Front-End: Node.JS, ReactJS e React Native.</p>
-            <a href="https://github.com/sjtorres">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/18393836?s=400&v=4" alt="Sandro Torres"></img>
-              <div className="user-info">
-                <strong>Sandro Torres</strong>
-                <span>ReactJS, React Native, Node.Js</span>
-              </div>
-            </header>
-            <p>Desenvolvedor Front-End: Node.JS, ReactJS e React Native.</p>
-            <a href="https://github.com/sjtorres">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/18393836?s=400&v=4" alt="Sandro Torres"></img>
-              <div className="user-info">
-                <strong>Sandro Torres</strong>
-                <span>ReactJS, React Native, Node.Js</span>
-              </div>
-            </header>
-            <p>Desenvolvedor Front-End: Node.JS, ReactJS e React Native.</p>
-            <a href="https://github.com/sjtorres">Acessar perfil no Github</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
